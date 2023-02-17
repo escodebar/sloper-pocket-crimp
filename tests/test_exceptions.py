28b4770438ae4df70c35ffce6511799e2ecff878
Pablo Escodebar <escodebar@gmail.com>
2023-02-17 20:46:22 +0100

    Verify that InvalidGesture is an exception
    The type of the exception could be chosen more precisely, depending on
    its usage. Since we have a test driven approach, its usage is not clear
    yet, and therefore the type of exception is not specified yet.

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -4,3 +4,8 @@ import pytest
 
 def test_InvalidGesture_exists():
     assert getattr(exceptions, "InvalidGesture", False)
+
+
+@pytest.mark.xfail
+def test_InvalidGesture_is_an_exception():
+    assert issubclass(exceptions.InvalidGesture, Exception)
