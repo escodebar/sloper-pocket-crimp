280f2285f05289044a14dd862b88477baa7e6621
Pablo Escodebar <escodebar@gmail.com>
2023-02-17 16:44:10 +0100

    Test outcome of game method with known gestures
    If we assume, that sloper-pocket-crimp is a library, then game would
    probably be part of the interface. In that case, these tests would be
    acceptance tests. I assume sloper-pocket-crimp to be a runable, which
    won't be used in other software, and therefore won't see game as part of
    the interface. I therfore don't mark these tests as acceptance tests.

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,25 @@
+from sloper_pocket_crimp import game
+from sloper_pocket_crimp import Gestures
+from sloper_pocket_crimp import Outcomes
 import pytest
 
 
-def test_import_game():
-    from sloper_pocket_crimp import game
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    "gesture_player_one, gesture_player_two, expected_outcome",
+    [
+        (Gestures.SLOPER, Gestures.SLOPER, Outcomes.DRAW),
+        (Gestures.SLOPER, Gestures.POCKET, Outcomes.PLAYER_TWO_WINS),
+        (Gestures.SLOPER, Gestures.CRIMP, Outcomes.PLAYER_ONE_WINS),
+        (Gestures.POCKET, Gestures.SLOPER, Outcomes.PLAYER_ONE_WINS),
+        (Gestures.POCKET, Gestures.POCKET, Outcomes.DRAW),
+        (Gestures.POCKET, Gestures.CRIMP, Outcomes.PLAYER_TWO_WINS),
+        (Gestures.CRIMP, Gestures.SLOPER, Outcomes.PLAYER_TWO_WINS),
+        (Gestures.CRIMP, Gestures.POCKET, Outcomes.PLAYER_ONE_WINS),
+        (Gestures.CRIMP, Gestures.CRIMP, Outcomes.DRAW),
+    ],
+)
+def test_game_outcomes(gesture_player_one, gesture_player_two, expected_outcome):
+    assert expected_outcome == game(
+        gesture_one=gesture_player_one, gesture_two=gesture_player_two
+    )
