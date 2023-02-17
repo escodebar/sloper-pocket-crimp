dbe285186f5df9a9052440800226d6ff05e853fc
Pablo Escodebar <escodebar@gmail.com>
2023-02-17 11:47:29 +0100

    Make the import test pass
    ...by implementing a function which does nothing.

--- sloper_pocket_crimp/__init__.py
+++ sloper_pocket_crimp/__init__.py
@@ -1,2 +1,3 @@
+from sloper_pocket_crimp.game import game
 from sloper_pocket_crimp.gestures import Gestures
 from sloper_pocket_crimp.outcomes import Outcomes
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,6 +1,5 @@
 import pytest
 
 
-@pytest.mark.xfail
 def test_import_game():
     from sloper_pocket_crimp import game
