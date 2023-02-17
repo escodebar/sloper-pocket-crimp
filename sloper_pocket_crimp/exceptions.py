f73060eb8bf664e6ed3d3ecd60aa29bb96838d88
Pablo Escodebar <escodebar@gmail.com>
2023-02-17 17:45:34 +0100

    Add the InvalidGesture exception
    ...although it's not an exception yet, the test will pass. We will deal
    with this in a later test.

---
+++ sloper_pocket_crimp/exceptions.py
@@ -0,0 +1,2 @@
+class InvalidGesture:
+    pass
--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -2,6 +2,5 @@ from sloper_pocket_crimp import exceptions
 import pytest
 
 
-@pytest.mark.xfail
 def test_InvalidGesture_exists():
     assert getattr(exceptions, "InvalidGesture", False)
