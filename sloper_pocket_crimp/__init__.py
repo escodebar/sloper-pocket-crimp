e945aca5f46fc57bc20a849448c2efb049ac8efd
Pablo Escodebar <escodebar@gmail.com>
2023-02-17 21:36:36 +0100

    Add empty computer player class

--- sloper_pocket_crimp/__init__.py
+++ sloper_pocket_crimp/__init__.py
@@ -1,3 +1,4 @@
 from sloper_pocket_crimp.game import game
 from sloper_pocket_crimp.gestures import Gestures
 from sloper_pocket_crimp.outcomes import Outcomes
+from sloper_pocket_crimp.players import ComputerPlayer
--- tests/test_players.py
+++ tests/test_players.py
@@ -1,6 +1,5 @@
 import pytest
 
 
-@pytest.mark.xfail
 def test_computer_player_class_exists():
     from sloper_pocket_crimp import ComputerPlayer
