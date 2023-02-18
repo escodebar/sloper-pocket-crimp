981002baacc5168df1ce47d40ce15e4a3015d640
Pablo Escodebar <escodebar@gmail.com>
2023-02-18 13:19:25 +0100

    Stabilize listing available gestures
    We wrote a spike in the previous commit to identify the needed methods.
    Now that we know what they are, we can write the tests to make them
    behave the way we expect them. Here I write a test to make sure that the
    print output listing the available gestures is appealing. I expect it to
    fail, since I didn't  work on the formatting of the output at all... and
    indeed it does fail.
    
    Next I can improve the method listing the available methods let the test
    pass.

--- tests/test_players.py
+++ tests/test_players.py
@@ -22,3 +22,28 @@ def human():
 def test_human_player_choses_a_gesture(monkeypatch, human, input_value):
     monkeypatch.setattr("builtins.input", lambda _: input_value)
     assert human.choice() in Gestures
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    "gestures, expected_print",
+    [
+        (
+            list(Gestures),
+            "Choose a gesture from the following list:\n"
+            "0: Sloper\n"
+            "1: Pocket\n"
+            "2: Crimp\n",
+        ),
+        (
+            list(Gestures)[:-1],
+            "Choose a gesture from the following list:\n" "0: Sloper\n" "1: Pocket\n",
+        ),
+    ],
+)
+def tests_list_available_gestures(human, gestures, expected_print, capsys):
+    human._list_available_gestures(gestures)
+
+    captured = capsys.readouterr()
+
+    assert expected_print == captured.out
