b21491cd90b3cedddb3a8979f100d1adba9917f2
Pablo Escodebar <escodebar@gmail.com>
2023-02-18 15:20:33 +0100

    Verify that choice can handle bad user input
    Ok... first things first. This test is bad. And it is bad for the sake
    of having a discussion. What's bad about the test? Quite a lot of
    things:
    - It tests more than one thing at the time
    - It tests knowing the implementation details (I prefer opaque box tests
      over transparent box tests)
    - There is implicit data in the test (it's not clear that 0, 1 and 2 are
      the indices for the expected gestures
    
    I wouldn't write this test this way but rather write two separate tests.
    One to test that the selected gesture is a valid gesture, then one for
    the number of iterations until the answer was valid. I would also use a
    library like hypothesis to generate invalid user input.
    
    Now looking at the bright and dangerous side of the test:
    This is the first time we use a MagicMock within this project. Magic
    mocks are fun and helpful, but on the other hand they're a smell.
    Overusing MagicMock can lead to tests not testing the software but
    rather the test setup. As mentioned before, I prefer having opaque box
    tests rather than transparent box tests. Is it just a matter of taste?

--- tests/test_players.py
+++ tests/test_players.py
@@ -1,6 +1,7 @@
 from sloper_pocket_crimp import ComputerPlayer
 from sloper_pocket_crimp import HumanPlayer
 from sloper_pocket_crimp import Gestures
+from unittest.mock import MagicMock
 import pytest
 
 
@@ -48,9 +49,35 @@ def tests_list_available_gestures(human, gestures, expected_print, capsys):
 
 
 @pytest.mark.parametrize("chosen_value", [0, 1, 2])
-def test_human_player_chose_gesture(monkeypatch, human, chosen_value):
+def test_human_player_chose_gesture(monkeypatch, human, chosen_value, gestures):
     monkeypatch.setattr("builtins.input", lambda _: str(chosen_value))
 
-    gestures = list(Gestures)
-
     assert gestures[chosen_value] == human._chose_gesture(gestures)
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize(
+    "chosen_values, expected_gesture",
+    [
+        ((-10, 0), Gestures.SLOPER),
+        (("x", "y", 1), Gestures.POCKET),
+        ((None, True, False, 2), Gestures.CRIMP),
+    ],
+)
+def test_human_is_repeteadly_asked_for_input_given_invalid_input(
+    monkeypatch,
+    human,
+    gestures,
+    chosen_values,
+    expected_gesture,
+):
+    list_available_gestures_mock = MagicMock()
+    monkeypatch.setattr(human, "_list_available_gestures", list_available_gestures_mock)
+
+    inputs = iter(chosen_values)
+    monkeypatch.setattr("builtins.input", lambda _: str(next(inputs)))
+
+    chosen_gesture = human.choice(gestures)
+
+    assert len(chosen_values) == len(list_available_gestures_mock.mock_calls)
+    assert expected_gesture == chosen_gesture
